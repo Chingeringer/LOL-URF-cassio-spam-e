9926a01873b420f7237a21512ee0bd9658a3fc09
Chingeringer <35901570+Chingeringer@users.noreply.github.com>
2023-01-01 16:55:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":#hold to spam
     pressing = False
     while True:  # making a loop
         time.sleep(random.expovariate(lambd=5))
-        if keyboard.is_pressed('space') and pressing == False:  # if key 'q' is pressed 
+        if keyboard.is_pressed('space') and pressing == False:  # if key 'space' is pressed 
             pressing = True
             keyboard.press_and_release('e')#spam key
         if keyboard.is_pressed('space') and pressing == True:
